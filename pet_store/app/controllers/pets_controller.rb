38460ee1dbace8ff0010aa3e57769eaae3da0b63
raidnaji <naji.raid@gmail.com>
2022-07-26 17:11:06 -0400

    first commit

---
+++ pet_store/app/controllers/pets_controller.rb
@@ -0,0 +1,21 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+    @pet = Pet.find(params[ :id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    @pet = Pet.new(params.require( :pet).permit( :name, :age, :description))
+    @pet.after_save
+    redirect_to @pet
+  end
+end
